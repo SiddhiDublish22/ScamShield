36f7a209d39cb628a78b6899fa1da31ce2732035
SiddhiDublish22 <siddhidublish.22@gmail.com>
2026-07-20 20:37:09 +0530

    Connect MongoDB Atlas database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("ScamShield Backend Running...");
+  res.send("ScamShield Backend Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
